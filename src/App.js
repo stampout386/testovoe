4009b0338d03fa97b966d75c8f935a638777b2bd
Mikhail Shestak <stampout386@gmail.com>
2022-07-28 19:18:24 +0300

    testovoe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [files, setFiles] = useState([])
+
+    const downloadFiles = () => {
+        fetch('http://aloteq-test-tasks.s3-website.eu-central-1.amazonaws.com/list.txt')
+            .then(response => response.text())
+            .then(data => {
+                const files = data.split(/\r?\n/)
+
+                return Promise.all(files.map(file => {
+                    return fetch(file)
+                        .then(response => {
+                            if (response.ok === false) {
+                                return {file: 'файл не найден'}
+                            } else {
+
+                                const name = new URL(file)
+                                return response.blob()
+                                    .then(file => {
+                                        return {fileName: name.pathname.slice(1), fileSize: response.headers.get('content-length'), file}
+                                    })
+
+                            }
+
+
+                        }).catch(error => console.log(error))
+                }))
+
+            })
+            .then(data => {
+                console.log(data)
+                setFiles(data)
+            })
+            .catch(console.log)
+
+
+    }
+
+    return (
+        <div className="App">
+            <button onClick={downloadFiles}>download</button>
+            <div>{files.map(file => <div>{file}</div>)}</div>
+        </div>
+    );
 }
 
 export default App;
